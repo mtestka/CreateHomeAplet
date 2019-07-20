9b2fec65749deafe6c374718317b868a02556843
Michał Testka <wswalters2@gmail.com>
2019-07-21 00:06:40 +0200

    Adding Taking Pics of Models and Saving as Sprites

---
+++ Assets/DragMoveObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragMoveObject : MonoBehaviour, IDragHandler, IEndDragHandler,IBeginDragHandler
+{
+    Vector3 initialPosition;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        initialPosition = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = initialPosition;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
