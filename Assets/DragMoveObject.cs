3bcc4fbc8c2352589f34f8b0ae9d1cd25cd3c124
Michał Testka <wswalters2@gmail.com>
2019-07-24 20:51:05 +0200

    Instantiate objects on wall from its UI pictures todo: -Add drag objects script to Instantiated elements

--- Assets/DragMoveObject.cs
+++ Assets/DragMoveObject.cs
@@ -5,21 +5,73 @@ using UnityEngine.EventSystems;
 
 public class DragMoveObject : MonoBehaviour, IDragHandler, IEndDragHandler,IBeginDragHandler
 {
+    public GameObject model3D;
     Vector3 initialPosition;
+    float objectScale = 0.0041f;
+    float objectPosX = 10.56f, objectPosY = 0, objectPosZ = 4.51f;
+    bool moving = false, instantiated = false;
+    GameObject instantiatedObject = null;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         initialPosition = transform.position;
+        moving = true;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = Input.mousePosition;
+        if (moving)
+        {
+            transform.position = Input.mousePosition;
+        }
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray,out hit, 1000))
+        {
+            switch (hit.collider.gameObject.name)
+            {
+                case "LeftWall":
+                    if (moving)
+                    {
+                        moving = false;
+                        instantiated = true;
+                        transform.position = initialPosition;
+                        instantiatedObject = Instantiate(model3D, new Vector3(10.56f, 0, 4.51f), Quaternion.identity);
+                        instantiatedObject.transform.localScale = new Vector3(objectScale, objectScale, objectScale);
+                        instantiatedObject.transform.localRotation = Quaternion.Euler(90, 0, 90);
+                    }
+                    else if(instantiatedObject != null)
+                    {
+                        instantiatedObject.transform.position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                    }
+                    break;
+                case "RightWall":
+                    break;
+                case "BackWall":
+                    break;
+                case "FrontWall":
+                    break;
+                default:
+                    break;
+            }
+            Debug.Log("You hit: " + hit.point);
+        }
+        else
+        {
+            moving = true;
+            if (instantiated)
+            {
+                Destroy(instantiatedObject);
+            }
+            Debug.Log("Is default state");
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         transform.position = initialPosition;
+        moving = false;
+        instantiated = false;
     }
 
     // Start is called before the first frame update
